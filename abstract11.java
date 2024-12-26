270b5e4585ef34d3598ac3a5f5d522e2a761de0a
anuguakhilesh <103905526+anuguakhilesh@users.noreply.github.com>
2024-12-25 19:23:10 -0500

    Add files via upload

---
+++ abstract11.java
@@ -0,0 +1,32 @@
+package javalearning;
+
+abstract class abstract11 //abstract class
+{
+  public abstract void animalsound();//abstract method does not have body
+  public void sleep()//regular method
+  {
+	  System.out.println("zzzzz");
+  }
+}
+class Pig extends abstract11
+{
+	public void animalsound()
+	{
+		System.out.println("wee weee");
+	}
+}
+class Dog extends abstract11
+{
+	public void animalsound()
+	{
+		System.out.println("bow bow");
+	}
+}
+class Main{
+	public static void main(String []args) {
+		Pig mypig=new Pig();//create pig object
+		Dog mydog=new Dog();
+		mypig.animalsound();
+		mydog.animalsound();
+	}
+}
